0e2934737e21de741d43f77b63410c619cdd2d8a
Benjamin Lautenbach <93003415+T-Benjamin-L@users.noreply.github.com>
2021-10-22 19:22:06 +0200

    Add files via upload

---
+++ OOP_Uebung01_Aufgabe03_Code.py
@@ -0,0 +1,14 @@
+eingabenList = []
+nutzerEingabe = int(input("Eingabe am Anfang, [0 zum stornieren]: "))
+eingabenList.append(nutzerEingabe)
+ersterTeil = nutzerEingabe
+while nutzerEingabe != 0:
+ nutzerEingabe = int(input("nächste Eingabe [0 zum stornieren]: "))
+ if nutzerEingabe == 0:
+ break
+ eingabenList.append(nutzerEingabe)
+ zweiterTeil = nutzerEingabe
+ print(f"Der Durchschnitt von {ersterTeil} und {zweiterTeil} ist {(ersterTeil + zweiterTeil)/2}")
+ ersterTeil = nutzerEingabe
+print(f"Der Durchschnittswert von allen eingegebenen Zahlen ist {sum(eingabenList) /
+(len(eingabenList))}")
